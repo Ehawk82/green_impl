830899780f9846147901c63314ade904a633e924
unknown <mrehawk82@gmail.com>
2022-03-23 14:26:48 -0400

    local storage initialized

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -1,4 +1,14 @@
 const init = () => {
+	var ad = parseLS("appData");
+	if(!ad || ad === null || ad === "undefined" || ad === undefined) {
+		saveLS("appData",appdata);
+		ad = parseLS("appData");
+		buildApp(ad);
+	} else {
+		buildApp(ad);
+	}	
+},
+buildApp = (ad) => {
 	
 };
 window.onload = () => {
---
+++ docs/src/js/stuff.js
@@ -0,0 +1,39 @@
+appdata = {
+	rookie: true,
+	level: 0,
+	tutorial: true,
+	media: {
+		sound: true,
+		volume: 1
+	},
+	dna: {},
+	mrna: {},
+	trna: {},
+	elements: {
+		oxygen: 0,
+		nitrogen: 0,
+		carbon: 0,
+		hydrogen: 0,
+		calcium: 0,
+		phosphorus: 0,
+		sodium: 0,
+		sulphur: 0,
+		chlorine: 0,
+		potassium: 0
+	},
+	compounds: {
+		water: 0,
+		carbohydrate: 0,
+		fat: 0,
+		protein: 0
+	},
+	aminos: {
+		adenine: 0,
+		guanine: 0,
+		cytosine: 0,
+		thymine: 0
+	},
+	nucleotides: 0,
+	codons: 0,
+	introns: 0
+};
